961e627d4857c3e18b36f1cc88f44fcc66706f0f
Sangeeth Adhikari <dilushasanga@gmail.com>
2018-07-18 22:39:43 +0800

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var dbContext = new StudentRegistrationEntitiesDBContext();
+            var students = dbContext.spGetAllStudents().ToList();
+            return View(students);
+        }
+        public ActionResult ViewStreet()
+        {
+            return RedirectToAction("ViewAddress");
+        }
+        public ActionResult ViewAddress()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Scripts/myApp.js
@@ -0,0 +1,4 @@
+﻿var app = angular.module('myApp',[]);
+    app.controller("HomeController", function ($scope) {
+        $scope.message = "my angular application with MVC";
+    });
